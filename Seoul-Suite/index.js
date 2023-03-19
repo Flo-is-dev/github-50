4b56d39297d11106586ac657c243c18d8d09e137
Flo-le-dev <f.gironde.dev@gmail.com>
2023-03-19 20:16:31 +0100

    Seoul V

---
+++ Seoul-Suite/index.js
@@ -0,0 +1,15 @@
+const btnToggle = document.querySelector(".btn-toggle");
+
+btnToggle.addEventListener("click", () => {
+  const body = document.body;
+
+  if (body.classList.contains("dark")) {
+    body.classList.add("light");
+    body.classList.remove("dark");
+    btnToggle.innerHTML = "DARK";
+  } else if (body.classList.contains("light")) {
+    body.classList.add("dark");
+    body.classList.remove("light");
+    btnToggle.innerHTML = "LIGHT";
+  }
+});
